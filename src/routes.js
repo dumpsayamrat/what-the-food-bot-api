0cbfb045f1ac65731aff56dcc12ace3e30857351
dumpsayamrat <sayamrat.kt@gmail.com>
2017-03-24 16:44:22 +0700

    Add get foods api with category

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import firebase from 'firebase';
 
-import { toQuickReply } from './functional';
+import { toQuickReply, shuffle, foodToGalleryElements } from './functional';
 
 const router = express.Router();
 
@@ -25,22 +25,50 @@ router.post('/categories/add', ({ body }, res) => {
   });
 });
 
-router.post('/categories/food/add', ({ body, query }, res) => {
-  const { category, foodName } = query;
+router.post('/categories/food/add', ({ query }, res) => {
+  const { category, foodName, image } = query;
   firebase.database().ref(`categories/${category}`).push({
-    name: foodName
+    name: foodName,
+    image
   }).then((response) => {
     res.json(`ADDED food ${foodName} in ${category} category`);
   });
 });
 
-router.get('/categories', ({ body }, res) => {
+router.get('/categories', (req, res) => {
   firebase.database().ref('/categories').on('value', snapshot => {
     res.json({
       messages: [
         {
+          text:  'ไหนลองเลือกประเภทอาหารดูก่อนละกัน',
           quick_replies: Object.keys(snapshot.val())
             .map(toQuickReply(['Food selecting'], 'type_of_food'))
+        }
+      ]
+    });
+  });
+});
+
+router.get('/foods', ({ query }, res) => {
+  const { category, limit = 3 } = query;
+  firebase.database().ref(`/categories/${category}`).on('value', snapshot => {
+    const obj = snapshot.val();
+    const foods= shuffle(Object.keys(obj))
+      .filter(k => obj[k].name != null)
+      .map(k => obj[k]);
+
+    res.json({
+      messages: [
+        {
+          attachment:{
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: foods
+                .slice(0, limit)
+                .map(foodToGalleryElements)
+            }
+          }
         }
       ]
     });
